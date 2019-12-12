d6967f2fcce0bcde8be9b68c6e703d87cb7a6246
Olesya Miller <olesyam.miller@gmail.com>
2019-12-12 02:44:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,17 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(list_of_ingredients)
-   cheese_types = ["cheddar", "gouda", "camembert"]
-   list_of_ingredients.find { |cheese| cheese_types.include?(cheese) }
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0 
+  while i < cheese_types.length 
+    if list_of_ingredients.include?(cheese_types[i])
+      return cheese_types[i]
+    end
+    i += 1 
+  end
+  nil
+  
+  # A way more elegant solution - list_of_ingredients.find { |cheese| cheese_types.include?(cheese) }
 end
+
+
